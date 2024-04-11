40b2a335043fcb4230439aaf6492294622e0b08a
Adam Fowler <adamfowler71@gmail.com>
2024-04-11 10:14:53 +0100

    Update script, add to README

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Template
 
 Template for Hummingbird App
+
+Run the following to clone locally
+```bash
+curl -L https://raw.githubusercontent.com/hummingbird-project/template/configure/scripts/download.sh | bash -s <project-name> 
+```
--- scripts/download.sh
+++ scripts/download.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 FOLDER=${1:-}
+TEMPLATE_VERSION=2.0.0-beta.1
+
 if [[ -z "$FOLDER" ]]; then
   echo "Missing folder name"
   echo "Usage: download.sh <folder>"
   exit 1
 fi
 
-curl -L https://codeload.github.com/hummingbird-project/template/tar.gz/refs/tags/2.0.0-beta.1 | tar xvz -s /template-2.0.0-beta.1/"$FOLDER"/
+curl -L https://codeload.github.com/hummingbird-project/template/tar.gz/refs/tags/"$TEMPLATE_VERSION" | tar xvz -s /template-2.0.0-beta.1/"$FOLDER"/
