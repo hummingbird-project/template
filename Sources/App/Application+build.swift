1b37ac726b255630916d58d84f05f7c366f3f5dc
Adam Fowler <adamfowler71@gmail.com>
2024-04-11 14:29:11 +0100

    Use package name not executable name

--- Sources/App/Application+build.swift
+++ Sources/App/Application+build.swift
@@ -13,7 +13,7 @@ public protocol AppArguments {
 
 public func buildApplication(_ arguments: some AppArguments) -> some ApplicationProtocol {
     let logger = {
-        var logger = Logger(label: "{{HB_EXECUTABLE_NAME}}")
+        var logger = Logger(label: "{{HB_PACKAGE_NAME}}")
         logger.logLevel = arguments.logLevel ?? .info
         return logger
     }()
@@ -26,7 +26,7 @@ public func buildApplication(_ arguments: some AppArguments) -> some Application
         router: router,
         configuration: .init(
             address: .hostname(arguments.hostname, port: arguments.port),
-            serverName: "{{HB_EXECUTABLE_NAME}}"
+            serverName: "{{HB_PACKAGE_NAME}}"
         ),
         logger: logger
     )
