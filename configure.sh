7d3209e0cbc63ad76c233a6457cd180bdf957b87
Adam Fowler <adamfowler71@gmail.com>
2025-04-19 17:48:52 +0100

    Add license header to configure script

--- configure.sh
+++ configure.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Hummingbird server framework project
+##
+## Copyright (c) 2021-2024 the Hummingbird authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
 PWD=$(pwd)
 TEMPLATE_FOLDER=$(dirname $0)
 RELATIVE_TARGET_FOLDER=${1:-}
